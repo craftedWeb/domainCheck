7d2c5dbc451d288ec054666a5d0e38f6af4cc877
craftedWeb <137318189+craftedWeb@users.noreply.github.com>
2023-06-28 23:59:00 +0200

    Create domaincheck.php

---
+++ domaincheck.php
@@ -0,0 +1,59 @@
+<?php
+require_once("assets/classes/TransipAPI.inc"); 
+<?php
+require_once("assets/classes/TransipAPI.inc");
+
+$domainStatuses = [
+	"inyouraccount" => "geregistreerd",
+	"unavailable" => "niet beschikbaar",
+	"notfree" => "geregistreerd",
+	"free" => "beschikbaar",
+	"internalpull" => "geregistreerdPull",
+	"internalpush" => "geregistreerdPush"
+];
+
+
+$domainName = strtolower($_POST["domain"]);
+$api = new TransipAPI();
+
+try {
+    $response = $api->checkDomain($domainName);
+
+    echo "<pre style='display: none' class='dev'>" . print_r($response, true) . "</pre>";
+
+    echo "<p>Gecontroleerde domeinnaam: " . $response->domainName . "</p>";
+    echo "<p>Status: " . $domainStatuses[$response->status] . "</p>";
+
+
+    if ($response->status === "free") {
+        echo "<form 
+                method='post' 
+                action=\"assets/functions/domains/bestelDomein.php/" . $response->domainName . "\">
+                <input type='submit' value='Bestel domeinnaam'>
+              </form>";
+    }
+    if ($response->status === "notfree" && in_array("transfer", $response->actions)) {
+        echo "<form 
+                method='post'
+                action=\"assets/functions/domains/verhuisDomein.php/" . $response->domainName . "\">
+                <input type='submit' value='Verhuis domeinnaam'>
+              </form>";
+    }
+    if ($response->status === "internalpush" && in_array("internalpush", $response->actions)) {
+        echo "<form
+                method='post'    
+                action=\"assets/functions/domains/verhuisDomeinPush.php/" . $response->domainName . "\">
+                <input type='submit' value='Verhuis aanvragen'>
+              </form>";
+    }
+    if ($response->status === "internalpull" && in_array("internalpull", $response->actions)) {
+        echo "<form 
+                method='post'
+                action=\"assets/functions/domains/verhuisDomeinPull.php/" . $response->domainName . "\">
+                <input type='submit' value='Verhuis aanvragen'>  
+              </p>";
+    }
+}
+catch (Exception $e) {
+	echo $e->getMessage();
+}
